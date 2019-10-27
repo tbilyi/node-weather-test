7273f3451058c13e99c2965e7b4a1fe76c81a9e7
Taras Bilyi <tbilyy@gmail.com>
2019-10-27 18:19:23 +0200

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=boston').then((response)=>{
+fetch('/weather?address=boston').then((response)=>{
 	response.json().then((data)=>{
 		if( data.error ){
 			console.log(`nooooo! ${data.error}`);
